952e816f00d441bccd15a2bd64961d8d9dd6adf4
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-02-28 17:40:45 +0000

    About created

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
+import icon1 from '../../assets/icon_1.svg';
+// import icon2 from '../../assets/icon2.svg';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero-container">
+      <div className="hero-content">
+      <img src={'icon2'} alt="Icon 2" className="hero-icon2" />
+        <div className="hero-text">
+          <h1 className="hero-title">
+            <span className="highlight-text">Ahmad</span> <span className="highlight-text-bahr">Bahr</span>
+          </h1>
+          <p className="hero-description">FrontEnd Web Developer</p>
+        </div>
+       
+      </div>
+      <img src={icon1} alt="Icon" className="hero-icon" />
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
