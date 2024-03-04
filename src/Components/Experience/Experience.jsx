96d1be9012985579f362605a2994de14e7a3f748
Ahmad <ahmad.bahr@mubs.edu.lb>
2024-03-04 22:29:37 +0200

    Creation of Experience

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './Experience.css'
+import React from 'react';
+import './Experience.css';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
+    <div className="experience-container">
+      {/* <h2 className="experience-header">Experience</h2> */}
+      <div className="experience-item">
+        <h3 className="experience-title"></h3>
+        <p className="experience-company"></p>
+        <p className="experience-duration"></p>
+        <ul className="experience-responsibilities">
+          <li>Developed responsive user interfaces using HTML, CSS, and JavaScript.</li>
+          <li>Collaborated with backend developers to integrate frontend with backend systems.</li>
+          <li>Implemented design mockups and wireframes into functional web applications.</li>
+        </ul>
+      </div>
+      <div className="experience-item">
+        <h3 className="experience-title"></h3>
+        <p className="experience-company"></p>
+        <p className="experience-duration"></p>
+        <ul className="experience-responsibilities">
+          <li>Assisted in the creation of wireframes, mockups, and prototypes.</li>
+          <li>Conducted user research and gathered feedback for iterative design improvements.</li>
+          <li>Collaborated with development team to ensure design feasibility and implementation.</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
