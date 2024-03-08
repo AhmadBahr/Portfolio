61eef0f2a26b5c9b8847a114e83c801f42d4033a
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-03-08 15:15:32 +0000

    working on the projects section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import './Projects.css';
+import icon_6 from '../../assets/2241285 1.svg'
+import icon_7 from '../../assets/depositphotos_574826670-stock-illustration-shopping-cart-simple-design 1.svg'
+import icon_8 from '../../assets/ecommerce-website-icon-vector 1.svg'
+import icon_9 from '../../assets/istockphoto-580129972-612x612.svg'
 
 const Projects = () => {
   const projectsData = [
     {
-      title: "Project 1",
-      description: "Description of project 1",
-      image: "project1.jpg",
-      subtitle: "Subtitle for Project 1",
-      technologies: ["React", "Node.js", "MongoDB"]
+      image: "icon_6",
+      title: "E-commerce Website",
+      description: " An intuitive e-commerce website with seamless navigation and secure payment processing",
+      technologies: ["HTML", "CSS", "JS","React"]
     },
     {
-      title: "Project 2",
-      description: "Description of project 2",
-      image: "project2.jpg",
-      subtitle: "Subtitle for Project 2",
-      technologies: ["JavaScript", "HTML", "CSS"]
+      image: "icon_7",
+      title: "Shopping Cart",
+      description: "Crafted an intuitive and visually appealing shopping cart UI with static props.",
+      technologies: ["HTML", "CSS"]
     },
     {
-      title: "Project 3",
-      description: "Description of project 3",
-      image: "project3.jpg",
-      subtitle: "Subtitle for Project 3",
-      technologies: ["React", "Redux", "Node.js"]
+      image: "icon_8",
+      title: "Rock Papers Scissors",
+      description: "Developed an engaging Rock-Paper-Scissors game with sleek UI design, smooth animations, and responsive controls, offering players a fun and immersive gaming experience.",
+      technologies: ["HTML", "CSS", "JS"]
     },
     {
-      title: "Project 4",
-      description: "Description of project 4",
-      image: "project4.jpg",
-      subtitle: "Subtitle for Project 4",
-      technologies: ["Python", "Django", "PostgreSQL"]
+      image: "icon_9",
+      title: "Super Ticktactoe Game",
+      description: "A captivating UI for a Super Tic-Tac-Toe game, incorporating strategic game play elements and interactive features to engage players.",
+      technologies: ["C#", "CSS", "HTML","JS","JQuery"]
     },
   ];
 
@@ -39,7 +39,7 @@ const Projects = () => {
       <div className="Projects-list">
         {projectsData.map((project, index) => (
           <div className="Project-item" key={index}>
-            <img src={require(`../../assets/${project.image}`).default} alt={project.title} className="Project-image" />
+            {/* <img src={require(`../../assets/${project.image}`).default} alt={project.title} className="Project-image" /> */}
             <div className="Project-details">
               <div className="Project-title">{project.title}</div>
               <div className="Project-subtitle">{project.subtitle}</div>
