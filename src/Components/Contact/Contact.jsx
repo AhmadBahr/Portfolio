09f9cd4a51e6eff1cd230c7f72be041505ce7e77
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-03-11 10:55:32 +0200

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faYoutube, faInstagram, faLinkedin, faTiktok } from '@fortawesome/free-brands-svg-icons';
@@ -11,20 +12,22 @@ const Contact = () => {
   };
 
   return (
-    <div id="contact" className="contact-container">
-      <h2 className="contact-title">Contact</h2>
-      <hr className="underline" />
-      <p className="contact-text">I am currently seeking employment or project offers.</p>
-      <p className="contact-text">Do not hesitate to contact me!</p>
-      <button className="message-button" onClick={handleWriteMessage}>Write Message</button>
-      <div className="social-links">
-        <a href="https://github.com/AhmadBahr"><FontAwesomeIcon icon={faGithub} /></a>
-        <a href="https://www.youtube.com/channel/UCo_E8q-0x4ad-FKgKqP0fVQ"><FontAwesomeIcon icon={faYoutube} /></a>
-        <a href="https://www.instagram.com/attotame"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a href="link-to-your-linkedin-profile"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="https://www.tiktok.com/@atto_toks"><FontAwesomeIcon icon={faTiktok} /></a>
+    <CSSTransition in={true} appear={true} timeout={500} classNames="fade">
+      <div id="contact" className="contact-container">
+        <h2 className="contact-title">Contact</h2>
+        <hr className="underline" />
+        <p className="contact-text">I am currently seeking employment or project offers.</p>
+        <p className="contact-text">Do not hesitate to contact me!</p>
+        <button className="message-button" onClick={handleWriteMessage}>Write Message</button>
+        <div className="social-links">
+          <a href="https://github.com/AhmadBahr"><FontAwesomeIcon icon={faGithub} /></a>
+          <a href="https://www.youtube.com/channel/UCo_E8q-0x4ad-FKgKqP0fVQ"><FontAwesomeIcon icon={faYoutube} /></a>
+          <a href="https://www.instagram.com/attotame"><FontAwesomeIcon icon={faInstagram} /></a>
+          <a href="link-to-your-linkedin-profile"><FontAwesomeIcon icon={faLinkedin} /></a>
+          <a href="https://www.tiktok.com/@atto_toks"><FontAwesomeIcon icon={faTiktok} /></a>
+        </div>
       </div>
-    </div>
+    </CSSTransition>
   );
 };
 
