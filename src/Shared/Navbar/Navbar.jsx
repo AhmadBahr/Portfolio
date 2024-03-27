ee12efc813a79ebd8250cd12bcab0659f3fb64c2
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-03-27 22:53:44 +0000

    CV updated

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import './Navbar.css'; 
-import Ahmad_CV from '../../assets/AhmadBahr_CurriculumVitae.pdf'; 
+import Ahmad_CV from '../../assets/AhmadBahr_ Curriculum Vitae.pdf'; 
 
 const Navbar = () => {
   const handleDownloadResume = () => {
