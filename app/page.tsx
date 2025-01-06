b6af4fb2e30d090cad0034e41d1a4dae5f075b5e
AhmadBahr <ahmad.bahr@mubs.edu.lb>
2025-01-06 19:12:41 +0200

    Add BentoGrid and Grid components for enhanced layout structure and organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from "@/components/Hero";
-
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/ui/Grid";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="mx-w-7xl w-full">
         <Hero />
+        {/* <FloatingNav /> */}
+        <Hero />
+        <Grid />
       </div>
     </main>
   );
