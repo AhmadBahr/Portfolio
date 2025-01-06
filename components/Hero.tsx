103bec20bfca443956677dedbe124d3a3a11084b
AhmadBahr <ahmad.bahr@mubs.edu.lb>
2025-01-06 15:35:55 +0200

    Add TextGenerateEffect component and enhance Hero layout with animated text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerate'
 
 const Hero = () => {
     return (
@@ -7,13 +8,18 @@ const Hero = () => {
             <div>
                 <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
                 <Spotlight className='-top-10 left-full fill-white h-[80vh] w-[50vw]' fill='purple' />
-                <Spotlight className='top-28 left-80 md:-left-32 h-[80vh] w-[50vw]' fill='full' />
+                <Spotlight className='top-28 left-80 md:-left-32 h-[80vh] w-[50vw]' fill='blue' />
             </div>
-            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             </div>
-            <div className='flex justify-center'>
-                
+            <div className='flex justify-center -relative my-20 z-10'>
+                <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                    <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Welcome to the future of web development</h2>
+                    <TextGenerateEffect
+                        className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                        words='We are a team of developers, designers, and marketers that are passionate about building the future of the web. We are here to help you build your dream website, app, or online store. We offer a wide range of services including web design, web development, e-commerce development, and digital marketing. Our team is dedicated to providing you with' />
+                </div>
             </div>
         </div>
     )
