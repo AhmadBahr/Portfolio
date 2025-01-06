c37d9a52dd15363a6aa622601197f95b24062c6d
AhmadBahr <ahmad.bahr@mubs.edu.lb>
2025-01-06 11:44:33 +0200

    Refactor Home component layout and add Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100">
-      <div>
-        <h1>Modern & Minimalist Portfolio</h1>
-        <p>My name is Ahmad Bahr.</p>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="mx-w-7xl w-full">
+        <h1>My name is Ahmad Bahr.</h1>
       </div>
     </main>
   );
