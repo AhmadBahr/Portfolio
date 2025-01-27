380d47e7a768eecfced5954d98717e33469f17e0
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2025-01-27 21:36:16 +0200

    update Hero.tsx and TextGenerateEffect.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
                         words="Transforming Concepts into Seamless User Experiences"
                         className="text-center text-[40px] md:text-5xl lg:text-6xl"
                     />
-                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white-100">
                         Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
                     </p>
                 </div>
