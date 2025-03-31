de336ae7b914f4404ac4e6f579606e3dd1efd5f7
Ahmad <ahmad.bahr@mubs.edu.lb>
2025-03-31 18:33:03 +0300

    ...........

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ export default withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "Ahmad Bahr",
+org: "javascript-mastery",
 project: "javascript-nextjs",
 }, {
 // For all available options, see:
